5276f95a9e2215a7b4f8d9b70ba7cd242a14f0f9
heath3rq <105904149+heath3rq@users.noreply.github.com>
2022-10-30 04:01:10 +0000

    add comments for python files

--- netflix_mv_ratings.py
+++ netflix_mv_ratings.py
@@ -1,3 +1,5 @@
+#!/usr/bin/env python3
+
 from sqlalchemy import create_engine
 import pandas as pd
 
@@ -7,18 +9,18 @@ netflix_mv_ratings = pd.read_csv('Netflix_datasets/netflix_movie_rating.csv', en
 # create a reference for sql library
 engine = create_engine('sqlite://',echo = False)
  
-# attach the data frame to the sql with a name of the table as "Employee_Data"
+# attach the data frame to the sql library and name the table as "mv_ratings"
 netflix_mv_ratings.to_sql('mv_ratings',con = engine)
 
-# show the complete data
-# from Employee_Data table
+# Query the sql table
+## Query 1: Find Netflix Original Films with an IMDB Score Greater than 8.5. 
 query1 = """SELECT Title, Genre, IMDB_Score
            FROM mv_ratings
            WHERE IMDB_Score > 8.5
            ORDER BY IMDB_Score DESC
            """
 greater_than_8 = engine.execute(query1).fetchall()
-
+## Query 2: Find the highest IMDb scores of the top 5 genre for Netflix Original Movies
 query2 = """SELECT DISTINCT Genre, max(IMDB_Score)
            FROM mv_ratings
            GROUP BY Genre
@@ -28,12 +30,14 @@ query2 = """SELECT DISTINCT Genre, max(IMDB_Score)
 
 highest_rating_genre = engine.execute(query2).fetchall()
 
+# Output to the console screen
+##From Query 1
 print('#'* 50)
-print(f"Netflix Original Films with IMDB Score Greater than 8.5:")
+print(f"Netflix Original Films with an IMDB Score Greater than 8.5:")
 for i in greater_than_8:
 	print(i)
-
+##From Query 2
 print('#'* 50)
-print(f"The top five genre with the highest IMDB Score of Netflix Original Movies are:")
+print(f"The highest IMDb Scores of the top 5 genre for Netflix Original Movies are:")
 for j in highest_rating_genre:
 	print(j)
--- netflix_titles.py
+++ netflix_titles.py
@@ -1,16 +1,16 @@
-# Import required modules
+#!/usr/bin/env python3
+
+## Import required modules
 import csv
 import sqlite3
 
-
-# Connecting to the geeks database
+## Connecting to the netflix_titles database
 connection = sqlite3.connect('netflix_titles.db')
 
-# Creating a cursor object to execute
-# SQL queries on a database table
+## Creating a cursor object to execute SQL queries on the netflix_titles database
 cursor = connection.cursor()
 
-## Table Definition
+## Defining table schema for the table, nfxtitle
 # create_table = '''CREATE TABLE nfxtitle(
 # 				show_id VAR PRIMARY KEY,
 # 				type VAR,
@@ -28,59 +28,54 @@ cursor = connection.cursor()
 # 				'''
 
 
-# # Creating the table into our
-# # database
+## Creating the table into the netflix_titles database
 # cursor.execute(create_table)
 
-# Opening the person-records.csv file
+## Opening the netflix_titles.csv
 file = open(
     'Netflix_datasets/netflix_titles.csv')
 
-# Reading the contents of the
-# person-records.csv file
+## Reading the contents of netflix_titles.csv
 contents = csv.reader(file)
 
-# # SQL query to insert data into the
-# # person table
+## SQL query to insert data into the nfxtitle table
 # insert_records = "INSERT INTO nfxtitle (show_id, type, title, director,cast,country,date_added,release_year,rating,duration,listed_in,description) VALUES(?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)"
 
-# # Importing the contents of the file
-# # into our person table
+## Importing the contents of the csv file into nfxtitle table
 # cursor.executemany(insert_records, contents)
 
-# SQL query to retrieve all data from
-# the person table To verify that the
-# data of the csv file has been successfully
-# inserted into the table
+## SQL queries to retrieve specified data from the nfxtitle table
+### Query 1: Find Netflix Original Films with an IMDB Score Greater than 8.5
 query1 = """SELECT Distinct country, count(title) 
 		   FROM nfxtitle
 		   WHERE country != ''
 		   Group by country
 		   Order by count(title) DESC
 		   LIMIT 10;"""
+top10_production_country = cursor.execute(query1).fetchall()
+### Query 2: Find the top five genre with the highest IMDB Score from Netflix Original Movies
 query2 = """SELECT Distinct type, release_year, count(title)
 		   FROM nfxtitle
 		   WHERE release_year != '' 
 		   AND release_year in ('2021', '2020', '2019', '2018', '2017', '2016', '2015','2014','2013','2012')
 		   Group by release_year
 		   Order by release_year DESC;"""
-
-top10_production_country = cursor.execute(query1).fetchall()
 top10_release_year = cursor.execute(query2).fetchall()
 
-# Output to the console screen
+## Output to the console screen
+###FROM Query 1
 print('#'* 50)
 print(f"BELOW ARE THE TOP 10 PRODUCTION COUNTRIES WITH THE MOST NETFLIX CONTENTS:")
 for i in top10_production_country:
     print(i)
-
+###FROM Query 2
 print('#'* 50)
 print(f"SHOW TYPE AND COUNT BREAK DOWN BY YEAR BETWEEN 2012 TO 2021:")
 for j in top10_release_year:
 	print(j)
 
-# Committing the changes
+## Committing the changes
 connection.commit()
 
-# closing the database connection
+## closing the database connection
 connection.close()
